a43ad4de218b49adc3f2d31bbf9c0f1840975d0d
Innosana <inno.chavalala@gmail.com>
2023-04-04 14:00:05 -0700

    0x13. C - More singly linked lists

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -6,3 +6,16 @@
  *
  * Return: resulting sum
  */
+int sum_listint(listint_t *head)
+{
+	int sum;
+
+	sum = 0;
+	while (head != NULL)
+	{
+		sum += head->n;
+		head = head->next;
+	}
+
+	return (sum);
+}
