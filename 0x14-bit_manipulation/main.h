92e0404594748a7fe84fc017c96ddefbd437843b
Innosana <inno.chavalala@gmail.com>
2023-04-11 14:00:54 -0700

    0x14. C - Bit manipulation

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,9 +1,11 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef ALX_H
+#define ALX_H
 
 unsigned int binary_to_uint(const char *b);
 
 void print_binary(unsigned long int n);
+
+int _putchar(char c);
 
 int get_bit(unsigned long int n, unsigned int index);
 
@@ -15,6 +17,5 @@ unsigned int flip_bits(unsigned long int n, unsigned long int m);
 
 int get_endianness(void);
 
-int _putchar(char c);
 
 #endif
