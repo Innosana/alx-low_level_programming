992a0634393db17a43a5b980ad2910065174022c
Innosana <inno.chavalala@gmail.com>
2023-05-06 15:27:56 -0700

    0x14-bit_manipulation

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -2,22 +2,19 @@
 #include <stdio.h>
 
 /**
- * flip_bits - calculate the number of bits that need to be flipped in num1 
- *             to obtain num2
- * @num1: the first number
- * @num2: the second number
- * Return: the number of bits that were flipped
+ * flip_bits - flip bits to convert one number to another number
+ * @num1: first number
+ * @num2: second number to convert to
+ * Return: number of bits that were needed to flip
  */
 unsigned int flip_bits(unsigned long int num1, unsigned long int num2)
 {
 	unsigned long int diff;
 	int counter;
 
-	/* XOR operation to find the bits that are different */
 	diff = num1 ^ num2;
 	counter = 0;
 
-	/* Count the number of set bits in the XOR result */
 	while (diff)
 	{
 		counter++;
