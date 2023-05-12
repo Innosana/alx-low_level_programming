7b7a01a0e93ec13315c25455ed25be5132181798
Innosana <inno.chavalala@gmail.com>
2023-05-12 12:28:32 -0700

    0x15-file_io

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,33 +1,38 @@
 #include "main.h"
 
 /**
- * create_file - Creates a file.
- * @file_name: A pointer to the name of the file to create.
- * @file_content: A pointer to a string to write to the file.
+ * create_file - creates a file
+ * @new_filename: new filename.
+ * @new_text_content: new content written in the file.
  *
- * Return: If the function fails - -1.
- *         Otherwise - 1.
+ * Return: 1 if it succeeds, -1 if it fails.
  */
-int create_file(const char *file_name, char *file_content)
+int create_file(const char *new_filename, char *new_text_content)
 {
-    int fd, bytes_written, len = 0;
+	int file_descriptor;
+	int num_letters;
+	int write_result;
 
-    if (file_name == NULL)
-        return (-1);
+	if (!new_filename)
+		return (-1);
 
-    if (file_content != NULL)
-    {
-        for (len = 0; file_content[len];)
-            len++;
-    }
+	file_descriptor = open(new_filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 
-    fd = open(file_name, O_CREAT | O_RDWR | O_TRUNC, 0600);
-    bytes_written = write(fd, file_content, len);
+	if (file_descriptor == -1)
+		return (-1);
 
-    if (fd == -1 || bytes_written == -1)
-        return (-1);
+	if (!new_text_content)
+		new_text_content = "";
 
-    close(fd);
+	for (num_letters = 0; new_text_content[num_letters]; num_letters++)
+		;
 
-    return (1);
+	write_result = write(file_descriptor, new_text_content, num_letters);
+
+	if (write_result == -1)
+		return (-1);
+
+	close(file_descriptor);
+
+	return (1);
 }
